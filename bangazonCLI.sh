81230ade8a1f71826e6b3a1ee4bf35a7a95b5b73
Dayne Wright <daynewr@gmail.com>
2016-10-21 10:29:26 -0500

    added path var for db on  migration delete

--- bangazonCLI.sh
+++ bangazonCLI.sh
@@ -36,7 +36,7 @@ elif [ "$1" == "restore" ]; then
   open -a 'Google Chrome' 'http://localhost:5000'
   dotnet run
 elif [ "$1" == "migration" ]; then
-  rm bangazon.db
+  rm $path_db
   rm -rf /Migrations
   dotnet ef migrations add Initial
   dotnet ef database update
