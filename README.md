0e00501755abf0bc974463ead436d797a6a72714
Matt Hamil <j.matthew.hamil@gmail.com>
2016-10-20 21:06:30 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bangazon Developer CLI
+
+## Installation
+
+Download the Bash script inside this repo to your machine and save it in your Bangazon directory (recommended).
+
+Open the script in a text editor and update the `PATH/TO/DATABASE.DB` line. Save the changes.
+
+Open your `~/.zshrc` file and create an alias that links to the Bash script like so:
+
+`alias bangazon="bash PATH/TO/bangazonDevCLI.sh"`
+
+Restart your terminal and run `bangazon` to show available commands.
---
+++ bangazonCLI.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export ASPNETCORE_ENVIRONMENT="Development"
+export BangazonWeb_Db_Path="PATH/TO/DATABASE.DB"
+if [[ $# -eq 0 ]]; then
+  echo -e "$(tput bold)██████╗  █████╗ ███╗   ██╗ ██████╗  █████╗ ███████╗ ██████╗ ███╗   ██╗
+██╔══██╗██╔══██╗████╗  ██║██╔════╝ ██╔══██╗╚══███╔╝██╔═══██╗████╗  ██║
+██████╔╝███████║██╔██╗ ██║██║  ███╗███████║  ███╔╝ ██║   ██║██╔██╗ ██║
+██╔══██╗██╔══██║██║╚██╗██║██║   ██║██╔══██║ ███╔╝  ██║   ██║██║╚██╗██║
+██████╔╝██║  ██║██║ ╚████║╚██████╔╝██║  ██║███████╗╚██████╔╝██║ ╚████║
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                      $(tput sgr0)"
+  echo -e "Bangazon CLI. Use $(tput bold)bangazon run$(tput sgr0) to run, $(tput bold)bangazon migration$(tput sgr0) if database migrations need to take place, or $(tput bold)bangazon restore$(tput sgr0) to restore dependencies."
+  exit 1
+elif [ "$1" == "run" ]; then
+  open -a 'Google Chrome' 'http://localhost:5000'
+  dotnet run
+elif [ "$1" == "restore" ]; then
+  dotnet restore
+  dotnet ef database update
+  bower install
+  open -a 'Google Chrome' 'http://localhost:5000'
+  dotnet run
+elif [ "$1" == "migration" ]; then
+  dotnet restore
+  rm -rf /Migrations
+  dotnet ef migrations add Initial
+  dotnet ef database update
+  bower install
+  open -a 'Google Chrome' 'http://localhost:5000'
+  dotnet run
+else echo "An error occured."
+fi
